60c0a70a11746faa5ef0038e97c12fec8078459f
Renato Trevisiol Montiel <renatotrevisiolmontiel@gmail.com>
2025-05-12 21:35:06 -0300

    Productos agregados al array original, junto al useEffect para mostrarlo en consola hecho por Renato Trevisiol Montiel

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/style.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Estados para los inputs
+  const [listaProductos, setListaProductos] = useState([]);
+  const [id, setId] = useState(0);
+  const [nombre, setNombre] = useState('');
+  const [descripcion, setDescripcion] = useState('');
+  const [precioUnitario, setPrecioUnitario] = useState('');
+  const [descuento, setDescuento] = useState('');
+  const [stock, setStock] = useState('');
+
+
+  //mostrar con el useEffect el array productos cada vez que se modifica (y 1 vez cuando se monta la página)
+  useEffect(()=>{
+    console.log(listaProductos);
+  }, [listaProductos])
+
+  const agregarProducto = (e)=>{
+    e.preventDefault();
+    if(nombre.trim() != '' && descripcion.trim() != '' && precioUnitario.trim() != '' && stock.trim()!=''){
+
+      //en caso de que el usuario no haya colocado un descuento
+      if(descuento.trim () == ''){
+        setDescuento('0')
+      }
+      
+      let productoNuevo = {
+        id: id,
+        nombre: nombre,
+        descripcion: descripcion,
+        precio: parseFloat(precioUnitario),
+        descuento: parseFloat(descuento),
+        stock: parseFloat(stock)
+      }
+
+      setListaProductos([...listaProductos, productoNuevo]);
+      
+      //incrementar el ID
+      setId(id +1);
+
+      //vaciar los campos de los inputs
+      setNombre('');
+      setDescripcion('');
+      setPrecioUnitario('');
+      setDescuento('');
+      setStock('');
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="contenedor">
+      <form>
+        <input
+          type="text"
+          disabled
+          placeholder={id}
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder='Nombre del producto'
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Descripción"
+          value={descripcion}
+          onChange={(e) => setDescripcion(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Precio Unitario"
+          value={precioUnitario}
+          onChange={(e) => setPrecioUnitario(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Descuento (%)"
+          value={descuento}
+          onChange={(e) => setDescuento(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Stock"
+          value={stock}
+          onChange={(e) => setStock(e.target.value)}
+        />
+        <button onClick={agregarProducto}>Agregar un producto</button>
+        <input type="text" placeholder="Buscar un producto" />
+        <h2>Productos</h2>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
